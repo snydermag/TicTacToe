7ef8409ba70baadc303f69c4e59d9311457babdd
maggie <snydermag@gmail.com>
2023-07-07 15:23:26 -0400

    Finished most Methods. Currently passing 30 tests

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,19 +4,98 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+
+    Character[][] board;
     public Board(Character[][] matrix) {
+        board = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        // Check Vertical
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                if (j == 2){
+                    return true;
+                }
+                if (board[j][i] == 'X' && board[j+1][i] == 'X'){
+                    continue;
+                }
+                else {
+                    break;
+                }
+            }
+        }
+
+        // Check Horizontal
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                if (j == 2){
+                    return true;
+                }
+                if (board[i][j] == 'X' && board[i][j+1] == 'X'){
+                    continue;
+                }
+                else {
+                    break;
+                }
+            }
+        }
+
+        // Check Diagonal
+        if ((board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') ||
+                (board[2][0] == 'X' && board[1][1] == 'X' && board[0][2] == 'X')){
+            return true;
+        }
+        return false;
+
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        // Check Vertical
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                if (j == 2){
+                    return true;
+                }
+                if (board[j][i] == 'O' && board[j+1][i] == 'O'){
+                    continue;
+                }
+                else {
+                    break;
+                }
+            }
+        }
+
+        // Check Horizontal
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                if (j == 2){
+                    return true;
+                }
+                if (board[i][j] == 'O' && board[i][j+1] == 'O'){
+                    continue;
+                }
+                else {
+                    break;
+                }
+            }
+        }
+
+        // Check Diagonal
+        if ((board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O') ||
+                (board[2][0] == 'O' && board[1][1] == 'O' && board[0][2] == 'O')){
+            return true;
+        }
+        return false;
     }
 
     public Boolean isTie() {
-        return null;
+        if ((isInFavorOfO() == true && isInFavorOfX() == true) || (isInFavorOfO() == false && isInFavorOfX() == false)){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     public String getWinner() {
